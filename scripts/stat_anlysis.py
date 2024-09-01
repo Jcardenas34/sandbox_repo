bea9354f5b52908dffdba89df61d8f5e9585045d
Juan Cardenas <cardenas.juan93@yahoo.com>
2024-09-01 13:01:03 -0700

    stat_analysis.py: Adding an input argument

--- scripts/stat_anlysis.py
+++ scripts/stat_anlysis.py
@@ -19,6 +19,7 @@ def main(args):
 
 if __name__ == "__main__":
 
-    
-    args = None
+    parser = argparse.ArgumentParser(description="For now a standard arg parse for input arguments")
+    parser.add_argument("infile", type=str, help="Input file path" )    
+    args = parser.parse_args()
     main(args)
