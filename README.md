fa2f20b608ef39c6e3a0ea6030853c99d7c6b78c
Juan Cardenas <cardenas.juan93@yahoo.com>
2024-09-01 12:39:03 -0700

    README.md: Adding text to the readme to describe the repo stat_analysis.py: Added a standard python script with a main function using the git amend functionality

---
+++ README.md
@@ -0,0 +1,3 @@
+# SandBox Repo
+The purpose of this git repo is to learn the complete functionality of git
+for use in larger projects
---
+++ scripts/stat_anlysis.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+
+'''
+Description:
+--------------
+This is another test file for git repo
+
+'''
+
+import argparse
+import os
+
+
+def main(args):
+
+    print("Testing")
+
+
+
+if __name__ == "__main__":
+
+    
+    args = None
+    main(args)
